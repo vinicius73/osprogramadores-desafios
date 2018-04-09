edb7408edb73ba2d7dba837557a32d371b94c356
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-04-09 20:24:28 -0300

    minor adjusts

--- src/pages/lib/load.js
+++ src/pages/lib/load.js
@@ -1,4 +1,4 @@
-import { reduce, get } from 'lodash'
+import { reduce, get, orderBy } from 'lodash'
 // export const rounds = ['10K', '50K', '100K', '250K', '500K', '1M', '2M', '3M', '5M', '8M', '12M', '15M', '20M', '25M', '30M']
 export const rounds = ['1M', '2M', '3M', '5M', '8M', '12M', '15M', '20M', '25M', '30M']
 
@@ -7,8 +7,8 @@ const getRounds = result => reduce(rounds, (acc, key) => {
   return acc
 }, [])
 
-export const loadResults = () => import('../../data/results.json')
-  .then(json => Object.keys(json)
+export const loadResults = () => import('../data/results.json')
+  .then(json => orderBy(Object.keys(json))
     .reduce((acc, key) => {
       acc.push([
         key,
@@ -18,7 +18,7 @@ export const loadResults = () => import('../../data/results.json')
     }, [])
   )
 
-export const loadTests = () => import('../../data/tests.json')
+export const loadTests = () => import('../data/tests.json')
   .then(json => Object.keys(json)
     .map(key => ({
       key,
